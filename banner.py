8ed7234010515803302b230fb7702098a3d630fc
cdead2 <70284481+cdead2@users.noreply.github.com>
2021-01-04 18:43:35 +0300

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,57 @@
+from colorama import Fore, Back, Style
+class Banner:
+    RED=Fore.RED
+    YELLOW=Fore.YELLOW
+    White=Fore.WHITE
+    GREEN=Fore.GREEN
+    Back=Back.GREEN
+#    def __init__(self):
+  #      self._help()
+      #  self.banner()
+       # self.Traingle()
+        #self._help()
+    def _help(self):
+        print (self.GREEN + "en >to encrypt a text")
+        print (self.RED + "de >to decrypt a text")
+        print (self.GREEN + "To encode_text:")
+        print (self.RED + "Example : python3 morse_decoder en hello world")
+        print (self.GREEN + "Output :  .... . .-.. .-..   .--   .-. .-.. -..")
+        print (self.RED + " To decode_morse:")
+        print (self.GREEN + "Example : python3 morse_decoder de .... . .-.. .-..   .--   .-. .-.. -..")
+        print (self.RED + "Output : hello world")
+
+    def banner(self):
+        print ( f"""
+{self.YELLOW}                           #     #                                 #####                       
+{self.GREEN} ##   ##  ####  #####   ####  ######    #     #  ####  #####  ###### 
+{self.RED} # # # # #    # #    # #      #         #       #    # #    # #      
+{self.YELLOW} #  #  # #    # #    #  ####  #####     #       #    # #    # #####  
+ #     # #    # #####       # #         #       #    # #    # #      
+ #     # #    # #   #  #    # #         #     # #    # #    # #      
+ #     #  ####  #    #  ####  ######     #####   ####  #####  ###### 
+                                                                    
+
+    """
+)
+    def Traingle(self):
+        n,m,b,v=6,0,6,7
+        for _ in range(1):
+            for i in range(1,n):
+                if i==1:
+                    print(' '*6+'*')
+            
+                print(' '*(n-i),end='')
+                print('*',' '*(i+m),end='')
+                print('*')
+        
+                m+=1
+            for h in range(n,0,-1):
+                print(' '*(n-h),end='')
+                print('*',' '*(h+b-2),end='')
+                print('*')
+                if h==1:
+                    print(' '*n+'*')
+                b-=1
+
+           
+Banner()
---
+++ morse_decoder.py
@@ -0,0 +1,51 @@
+from banner import Banner
+import sys,huepy,colorama
+morse={'A':'.-','B':'-...','C':'-.-.','D':'-..','E':'.','F':'..-.','G':'--.','H':'....','I':'..','J':'.---','K':'-.-','L':'.-..','M':'--','N':'-.','P':'.--.','Q':'--.-','O':'---','R':'.-.','S':'...','T':'-','U':'..-','V':'...-','W':'.--','X':'-..-','Y':'-.--','Z':'--..','.':'.-.-.-',',':'--..--',';':'-.-.-.','?':'..--..','!':'-.-.--'}
+def morse_encoder(text):
+
+    date=text
+    morse_data=[]
+    for i in date:
+        try:
+            i=i.upper()
+            morse_data.append(morse[i])
+        except:
+            morse_data.append(' ')
+    return morse_data
+revers_={}
+def morse_decoder(morse_,morse):
+    for i in morse:
+        revers_[morse[i]]=i
+    data=[]
+    for i in morse_:
+        try:
+            
+            data.append(revers_[i])
+        except:
+            data.append(i)
+            #pass
+    return data
+   
+
+
+try:
+    choose=sys.argv[1]
+    text=sys.argv[2:]
+    if choose=='en':
+        text=''.join(text)
+        text=list (text)
+        print (huepy.yellow('Your morse encryption : '+' '.join(morse_encoder(text))))
+    elif choose=='de':
+        text=sys.argv[2::]
+        print(huepy.green('Your decrypted text : '+' '.join(morse_decoder(text,morse))).lower())
+    else:
+        OOb=Banner()
+        OOb.banner()
+        OOb.Triangle()
+        OOb._help()
+
+except:
+    OOb=Banner()
+    OOb.banner()
+    OOb.Traingle()
+    OOb._help()
